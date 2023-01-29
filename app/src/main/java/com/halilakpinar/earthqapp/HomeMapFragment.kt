44f8025fb696320c962adcb6dc49234cce82c484
Halil Akpınar <74452544+hkpnrr@users.noreply.github.com>
2023-01-29 21:14:04 +0300

    HomeMapFragment oluşturuldu. CurrentLocation haritada gösteriliyor.

--- app/src/main/java/com/halilakpinar/earthqapp/HomeMapFragment.kt
+++ app/src/main/java/com/halilakpinar/earthqapp/HomeMapFragment.kt
@@ -4,8 +4,9 @@ import android.content.Context
 import android.content.pm.PackageManager
 import android.location.LocationListener
 import android.location.LocationManager
-import android.os.Bundle
 import androidx.fragment.app.Fragment
+
+import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -15,11 +16,16 @@ import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.navigation.Navigation
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationRequest
 import com.google.android.gms.location.LocationServices
+
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import com.google.android.gms.tasks.CancellationTokenSource
 import com.google.android.material.snackbar.Snackbar
 import com.halilakpinar.earthqapp.Adapter.RecyclerViewAdapter
@@ -29,23 +35,21 @@ import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.fragment_home.*
+import kotlinx.android.synthetic.main.fragment_home_map.*
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
+class HomeMapFragment : Fragment() {
 
-class HomeFragment : Fragment() {
-
+    private lateinit var mapFragment: SupportMapFragment
     private val BASE_URL="https://earthquake.usgs.gov/fdsnws/event/1/"
-    private var compositeDisposable:CompositeDisposable?=null
-
-
+    private var compositeDisposable: CompositeDisposable?=null
     private lateinit var locationManager:LocationManager
     private lateinit var locationListener: LocationListener
-
-    private lateinit var permissionLauncher:ActivityResultLauncher<String>
+    private lateinit var permissionLauncher: ActivityResultLauncher<String>
 
     private var currentLatitude:Double?=null
     private var currentLongitude:Double?=null
@@ -55,47 +59,75 @@ class HomeFragment : Fragment() {
 
     private lateinit var fusedLocationClient: FusedLocationProviderClient
 
-    private var recyclerViewAdapter:RecyclerViewAdapter?=null
+    private val callback = OnMapReadyCallback { googleMap ->
 
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
+        val sydney = LatLng(currentLatitude!!, currentLongitude!!)
+        googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
-
+        return inflater.inflate(R.layout.fragment_home_map, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        mapFragment = (childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?)!!
         compositeDisposable= CompositeDisposable()
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireActivity())
 
-        val layoutManager: RecyclerView.LayoutManager=LinearLayoutManager(requireContext())
-        recyclerView.layoutManager=layoutManager
-
-
-        registerLauncher()
-
-        getCurrentLocation()
-
-
-        floatingActionButton.setOnClickListener {
-            val action = HomeFragmentDirections.actionHomeFragmentToHomeMapFragment()
+        floatingActionButton2.setOnClickListener {
+            val action = HomeMapFragmentDirections.actionHomeMapFragmentToHomeFragment()
             Navigation.findNavController(it).navigate(action)
         }
-
-
+        registerLauncher()
+        getCurrentLocation()
 
     }
 
+    fun getCurrentLocation(){
+        locationManager= requireActivity().getSystemService(Context.LOCATION_SERVICE) as LocationManager
 
+
+        if(ContextCompat.checkSelfPermission(requireActivity().applicationContext,android.Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED){
+            if(ActivityCompat.shouldShowRequestPermissionRationale(requireActivity(),android.Manifest.permission.ACCESS_FINE_LOCATION)){
+                Snackbar.make(requireView(),"Permission needed for location", Snackbar.LENGTH_INDEFINITE).setAction("Give Permission"){
+                    //request permission
+                    permissionLauncher.launch(android.Manifest.permission.ACCESS_FINE_LOCATION)
+                }.show()
+            }else{
+
+                //request permission
+                permissionLauncher.launch(android.Manifest.permission.ACCESS_FINE_LOCATION)
+            }
+        }else{
+            //permission granted
+            //locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,10000,0f,locationListener)
+            fusedLocationClient.getCurrentLocation(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY, CancellationTokenSource().token)
+                .addOnSuccessListener { location ->
+                    currentLatitude=location.latitude
+                    currentLongitude=location.longitude
+                    val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")
+                    endDate = LocalDateTime.now().format(formatter)
+                    startDate = LocalDateTime.now().minusDays(7).format(formatter)
+
+                    println(currentLatitude)
+                    println(currentLongitude)
+                    loadData()
+
+                }
+                .addOnFailureListener { exception ->
+
+                    Toast.makeText(requireContext(),exception.localizedMessage, Toast.LENGTH_LONG).show()
+                }
+        }
+
+        //locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,10000,0f,locationListener)
+    }
 
     fun registerLauncher(){
 
@@ -131,51 +163,9 @@ class HomeFragment : Fragment() {
         }
     }
 
-    fun getCurrentLocation(){
-        locationManager= requireActivity().getSystemService(Context.LOCATION_SERVICE) as LocationManager
-
-
-        if(ContextCompat.checkSelfPermission(requireActivity().applicationContext,android.Manifest.permission.ACCESS_FINE_LOCATION)!=PackageManager.PERMISSION_GRANTED){
-            if(ActivityCompat.shouldShowRequestPermissionRationale(requireActivity(),android.Manifest.permission.ACCESS_FINE_LOCATION)){
-                Snackbar.make(requireView(),"Permission needed for location",Snackbar.LENGTH_INDEFINITE).setAction("Give Permission"){
-                    //request permission
-                    permissionLauncher.launch(android.Manifest.permission.ACCESS_FINE_LOCATION)
-                }.show()
-            }else{
-
-                //request permission
-                permissionLauncher.launch(android.Manifest.permission.ACCESS_FINE_LOCATION)
-            }
-        }else{
-            //permission granted
-            //locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,10000,0f,locationListener)
-            fusedLocationClient.getCurrentLocation(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY, CancellationTokenSource().token)
-                .addOnSuccessListener { location ->
-                    currentLatitude=location.latitude
-                    currentLongitude=location.longitude
-                    val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")
-                    endDate = LocalDateTime.now().format(formatter)
-                    startDate = LocalDateTime.now().minusDays(7).format(formatter)
-
-                    println(currentLatitude)
-                    println(currentLongitude)
-                    loadData()
-
-                }
-                .addOnFailureListener { exception ->
-
-                    Toast.makeText(requireContext(),exception.localizedMessage,Toast.LENGTH_LONG).show()
-                }
-        }
-
-        //locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,10000,0f,locationListener)
-    }
-
-
-
     fun loadData(){
 
-        val retrofit=Retrofit.Builder()
+        val retrofit= Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
             .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
@@ -217,7 +207,7 @@ class HomeFragment : Fragment() {
 
     }
 
-    private fun handleResponse(response:NestedJSONModel){
+    private fun handleResponse(response: NestedJSONModel){
         response?.let {
             println(response.metadata.url)
             println(response.features.get(0).properties.place)
@@ -225,9 +215,9 @@ class HomeFragment : Fragment() {
             println(response.features.get(0).properties.time)
             println(currentLatitude)
             println(currentLongitude)
+            mapFragment?.getMapAsync(callback)
 
-            recyclerViewAdapter= RecyclerViewAdapter(response.features)
-            recyclerView.adapter=recyclerViewAdapter
+
 
         }
 
@@ -237,6 +227,4 @@ class HomeFragment : Fragment() {
         super.onDestroy()
         compositeDisposable?.clear()
     }
-
-
 }
