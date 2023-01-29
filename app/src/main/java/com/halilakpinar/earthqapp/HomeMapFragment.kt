147739d935e808f7f1316005b4b8899418a5997f
Halil Akpınar <74452544+hkpnrr@users.noreply.github.com>
2023-01-29 22:13:05 +0300

    Konuma göre yakın tarihteki gerçekleşen depremler liste ve harita görünümü

--- app/src/main/java/com/halilakpinar/earthqapp/HomeMapFragment.kt
+++ app/src/main/java/com/halilakpinar/earthqapp/HomeMapFragment.kt
@@ -1,6 +1,8 @@
 package com.halilakpinar.earthqapp
 
+import android.annotation.SuppressLint
 import android.content.Context
+import android.content.DialogInterface
 import android.content.pm.PackageManager
 import android.location.LocationListener
 import android.location.LocationManager
@@ -13,6 +15,7 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.navigation.Navigation
@@ -21,24 +24,23 @@ import com.google.android.gms.location.LocationRequest
 import com.google.android.gms.location.LocationServices
 
 import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.google.android.gms.tasks.CancellationTokenSource
 import com.google.android.material.snackbar.Snackbar
-import com.halilakpinar.earthqapp.Adapter.RecyclerViewAdapter
+import com.halilakpinar.earthqapp.Model.FeaturesModel
 import com.halilakpinar.earthqapp.Model.NestedJSONModel
 import com.halilakpinar.earthqapp.Service.EarthquakeAPI
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.schedulers.Schedulers
-import kotlinx.android.synthetic.main.fragment_home.*
 import kotlinx.android.synthetic.main.fragment_home_map.*
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
+import java.text.SimpleDateFormat
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
@@ -57,13 +59,46 @@ class HomeMapFragment : Fragment() {
     private var startDate:String?=null
     private var endDate:String?=null
 
+    private lateinit var dataList: NestedJSONModel
+
     private lateinit var fusedLocationClient: FusedLocationProviderClient
 
+    @SuppressLint("MissingPermission")
     private val callback = OnMapReadyCallback { googleMap ->
 
-        val sydney = LatLng(currentLatitude!!, currentLongitude!!)
-        googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        val userLocation = LatLng(currentLatitude!!, currentLongitude!!)
+        googleMap.isMyLocationEnabled=true
+
+        for (feature:FeaturesModel in dataList.features){
+            googleMap.addMarker(MarkerOptions().position(LatLng(feature.geometry.coordinates[1],feature.geometry.coordinates[0])).title(feature.id))
+            println(feature.properties.place)
+        }
+        //googleMap.addMarker(MarkerOptions().position(LatLng(37.0,27.0)).title("User1"))
+        //googleMap.addMarker(MarkerOptions().position(LatLng(36.0,25.0)).title("User2"))
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userLocation,7f))
+
+        googleMap.setOnMarkerClickListener {
+            val builder = AlertDialog.Builder(requireContext())
+            for (feature:FeaturesModel in dataList.features){
+                if(feature.id == it.title){
+                    with(builder)
+                    {
+                        val sdf = SimpleDateFormat("dd/MM/yy hh:mm a")
+                        val date =sdf.format(feature.properties.time)
+                        setTitle(feature.properties.place)
+                        setMessage("Magnitude: "+feature.properties.mag+"\n"+"Time: "+date+"\n"+"Coordinates: "+feature.geometry.coordinates[0].toString()+" "+feature.geometry.coordinates[1].toString()+"\n"+"Alert Level : "+feature.properties.alert)
+                        /*setPositiveButton("OK", DialogInterface.OnClickListener(function = positiveButtonClick))
+                        setNegativeButton(android.R.string.no, negativeButtonClick)
+                        setNeutralButton("Maybe", neutralButtonClick)*/
+                        show()
+                    }
+                }
+            }
+
+            Toast.makeText(requireContext(),it.title,Toast.LENGTH_SHORT).show()
+
+            return@setOnMarkerClickListener false
+        }
     }
 
     override fun onCreateView(
@@ -91,7 +126,6 @@ class HomeMapFragment : Fragment() {
 
     fun getCurrentLocation(){
         locationManager= requireActivity().getSystemService(Context.LOCATION_SERVICE) as LocationManager
-
 
         if(ContextCompat.checkSelfPermission(requireActivity().applicationContext,android.Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED){
             if(ActivityCompat.shouldShowRequestPermissionRationale(requireActivity(),android.Manifest.permission.ACCESS_FINE_LOCATION)){
@@ -215,9 +249,8 @@ class HomeMapFragment : Fragment() {
             println(response.features.get(0).properties.time)
             println(currentLatitude)
             println(currentLongitude)
+            dataList=response
             mapFragment?.getMapAsync(callback)
-
-
 
         }
 
